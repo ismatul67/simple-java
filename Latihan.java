22a35a41c5ce3615c9a30d1f0ba638d32713e093
isma67 <ismatulmaula67@gmail.com>
2020-11-26 22:32:30 +0700

    initial commit

---
+++ Latihan.java
@@ -0,0 +1,10 @@
+public class Latihan{
+
+  public static void main(String[] args) {
+	// write your code here
+
+        String hallo ="Hello World";
+
+        System.out.println(hallo);
+    }
+}
